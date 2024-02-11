308c01adc71f07ddcf6d6f8251d329f54acd3a52
Yassin <rebai.yassin23.yr@gmail.com>
2024-02-11 17:19:26 +0100

    Make the Photo data class Parcelable so that it can be properly serialized for the purpose of passing it between activities or storing it in the saved instance state.

--- app/src/main/java/com/sample/photogalleryapplication/model/Photo.kt
+++ app/src/main/java/com/sample/photogalleryapplication/model/Photo.kt
@@ -1,9 +1,13 @@
 package com.sample.photogalleryapplication.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 /** Data class that represents a Reddit photo. */
+@Parcelize
 data class Photo(
     val imageUrl: String,
     val title: String,
     val subreddit: String,
     val upVotes: String,
-)
+)  : Parcelable
