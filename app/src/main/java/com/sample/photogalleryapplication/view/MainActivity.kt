b311d42c3ab4d52c54a3bacf5ef735a02dd6b74a
Yassin <rebai.yassin23.yr@gmail.com>
2024-02-11 17:47:36 +0100

    Organize UI code further by defining the Photo Grid Page.

--- app/src/main/java/com/sample/photogalleryapplication/view/MainActivity.kt
+++ app/src/main/java/com/sample/photogalleryapplication/view/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.sample.photogalleryapplication.view
 
 import android.os.Bundle
-import androidx.compose.runtime.*
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.sample.photogalleryapplication.view.theme.PhotoGalleryApplicationTheme
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.unit.dp
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import com.sample.photogalleryapplication.model.Photo
-import com.sample.photogalleryapplication.model.UiResult
 import com.sample.photogalleryapplication.di.networkModule
 import com.sample.photogalleryapplication.di.viewModelModule
 import com.sample.photogalleryapplication.di.repositoryModule
@@ -39,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    PhotoGalleryApp(viewModel)
+                    ShowPhotoGalleryPage(viewModel) {}
                 }
             }
         }
@@ -78,62 +69,3 @@ class MainActivity : ComponentActivity() {
 //        }
 //    }
 //}
-
-@Composable
-fun PhotoGalleryApp(viewModel: PhotoGalleryViewModel) {
-
-    var uiResult by remember { mutableStateOf<UiResult<List<Photo>>?>(null) }
-
-    var searchText by rememberSaveable { mutableStateOf("") }
-
-    var photos by rememberSaveable { mutableStateOf<List<Photo>>(emptyList()) }
-
-    /** LiveData observer configuration. */
-    viewModel.photos.observe(LocalLifecycleOwner.current) { uiResult = it }
-
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        // Search box
-        OutlinedTextField(
-            value = searchText,
-            onValueChange = { newText ->
-                searchText = newText
-                viewModel.searchPhotos(newText)
-            },
-            label = { Text("Search") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        )
-
-        /** Show the adequate element based on the UI Result. */
-        when (val result = uiResult) {
-            is UiResult.Loading -> {
-                ShowProgressBar()
-            }
-
-            is UiResult.Success -> {
-                photos = result.data
-                if (photos.isNotEmpty())
-                    ShowPhotoGrid(photos) {
-                        // TODO: Add logic to navigate to details page here.
-                    }
-                else
-                    ShowEmptyResponse()
-            }
-
-            is UiResult.Error -> {
-                if (result.isNetworkError) {
-                    ShowErrorResponse()
-                } else {
-                    ShowEmptyResponse()
-                }
-            }
-
-            else -> {
-                ShowEmptyResponse()
-            }
-        }
-    }
-}
--- app/src/main/java/com/sample/photogalleryapplication/view/PhotoGrid.kt
+++ app/src/main/java/com/sample/photogalleryapplication/view/PhotoGrid.kt
@@ -15,17 +15,84 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.sample.photogalleryapplication.model.Photo
+import com.sample.photogalleryapplication.model.UiResult
+import com.sample.photogalleryapplication.viewmodel.PhotoGalleryViewModel
+
+
+@Composable
+fun ShowPhotoGalleryPage(viewModel: PhotoGalleryViewModel, onItemClick: (Photo) -> Unit) {
+
+    var uiResult by remember { mutableStateOf<UiResult<List<Photo>>?>(null) }
+
+    var searchText by rememberSaveable { mutableStateOf("") }
+
+    var photos by rememberSaveable { mutableStateOf<List<Photo>>(emptyList()) }
+
+    /** LiveData observer configuration. */
+    viewModel.photos.observe(LocalLifecycleOwner.current) { uiResult = it }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Search box
+        OutlinedTextField(
+            value = searchText,
+            onValueChange = { newText ->
+                searchText = newText
+                viewModel.searchPhotos(newText)
+            },
+            label = { Text("Search") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+
+        /** Show the adequate element based on the UI Result. */
+        when (val result = uiResult) {
+            is UiResult.Loading -> {
+                ShowProgressBar()
+            }
+
+            is UiResult.Success -> {
+                photos = result.data
+                if (photos.isNotEmpty())
+                    ShowPhotoGrid(photos, onItemClick)
+                else
+                    ShowEmptyResponse()
+            }
+
+            is UiResult.Error -> {
+                if (result.isNetworkError) {
+                    ShowErrorResponse()
+                } else {
+                    ShowEmptyResponse()
+                }
+            }
+
+            else -> {
+                ShowEmptyResponse()
+            }
+        }
+    }
+}
 
 @Composable
 fun ShowPhotoGrid(photosToShow: List<Photo>, onItemClick: (Photo) -> Unit) {
