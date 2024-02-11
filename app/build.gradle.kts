15e94292b32f2cfb792fc6c9088a802520590dad
Yassin <rebai.yassin23.yr@gmail.com>
2024-02-11 22:19:09 +0100

    Add navigation-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Coil
     implementation("io.coil-kt:coil-compose:2.5.0")
