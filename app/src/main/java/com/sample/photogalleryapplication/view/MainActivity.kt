38097d4456ec4e80753b3f237f4a0e6e26939b33
Yassin <rebai.yassin23.yr@gmail.com>
2024-02-11 22:23:46 +0100

    Add navigation support between the ShowPhotoGalleryPage and the DetailPage. The DetailPage needs the photo data, this is done by converting the selected photo data to JSON and then passing it in the nav route to finally parse it from the BackStack arguments and pass it to show the photo details.

--- app/src/main/java/com/sample/photogalleryapplication/view/MainActivity.kt
+++ app/src/main/java/com/sample/photogalleryapplication/view/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.sample.photogalleryapplication.view
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.gson.Gson
 import com.sample.photogalleryapplication.view.theme.PhotoGalleryApplicationTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import com.sample.photogalleryapplication.di.networkModule
 import com.sample.photogalleryapplication.di.viewModelModule
 import com.sample.photogalleryapplication.di.repositoryModule
+import com.sample.photogalleryapplication.model.Photo
 import com.sample.photogalleryapplication.viewmodel.PhotoGalleryViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -20,17 +28,36 @@ import org.koin.core.logger.Level
 class MainActivity : ComponentActivity() {
 
     private val viewModel: PhotoGalleryViewModel by viewModel()
+    private val gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startKoinIfNeeded()
         setContent {
-            PhotoGalleryApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ShowPhotoGalleryPage(viewModel) {}
+            val navController = rememberNavController()
+
+            NavHost(navController, startDestination = ROUTE_PHOTO_GALLERY_PAGE) {
+                composable(ROUTE_PHOTO_GALLERY_PAGE) {
+                    PhotoGalleryApplicationTheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            ShowPhotoGalleryPage(viewModel) { photo ->
+                                navigateToDetailPage(navController, photo)
+                            }
+                        }
+                    }
+                }
+                composable("$ROUTE_PHOTO_DETAIL_PAGE/{$PHOTO_JSON}") { backStackEntry ->
+                    PhotoGalleryApplicationTheme {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colorScheme.background
+                        ) {
+                            DetailPage(navController, backStackEntry.parsePhoto())
+                        }
+                    }
                 }
             }
         }
@@ -52,20 +79,22 @@ class MainActivity : ComponentActivity() {
             }
     }
 
-    companion object {
-        private var koinStarted = false
+    /** Function to navigate to the detail screen with Photo object. */
+    private fun navigateToDetailPage(navController: NavHostController, photo: Photo) {
+        navController.navigate("$ROUTE_PHOTO_DETAIL_PAGE/${Uri.encode(gson.toJson(photo))}")
+    }
+
+    /**
+     * Parses the [Photo] object from the arguments of the [NavBackStackEntry].
+     * @return The parsed [Photo] object, or null if parsing fails.
+     */
+    private fun NavBackStackEntry.parsePhoto() =
+        gson.fromJson(arguments?.getString(PHOTO_JSON), Photo::class.java)
+
+    private companion object {
+        var koinStarted = false
+        const val ROUTE_PHOTO_GALLERY_PAGE = "PhotoGalleryPage"
+        const val ROUTE_PHOTO_DETAIL_PAGE = "photoDetailPage"
+        const val PHOTO_JSON = "photoJson"
     }
 }
-
-//@Preview(showBackground = true)
-//@Composable
-//fun PhotoGalleryAppPreview() {
-//    PhotoGalleryApplicationTheme {
-//        Surface(
-//            modifier = Modifier.fillMaxSize(),
-//            color = MaterialTheme.colorScheme.background
-//        ) {
-//            PhotoGalleryApp()
-//        }
-//    }
-//}
