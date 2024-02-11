d3cadc0190fd32b7df390a8b2479353e0c13f707
Yassin <rebai.yassin23.yr@gmail.com>
2024-02-11 12:07:43 +0100

    update code using the created UiResult class, for this the photos attribute in the viewModel is now using liveData for online updates.
    Added lifecycle-livedata dependency.

--- app/src/main/java/com/sample/photogalleryapplication/viewmodel/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/sample/photogalleryapplication/viewmodel/PhotoGalleryViewModel.kt
@@ -1,41 +1,58 @@
 package com.sample.photogalleryapplication.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.asLiveData
 import com.sample.photogalleryapplication.model.Photo
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.sample.photogalleryapplication.model.UiResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 /** The [ViewModel] for fetching a list of top [Photo] from a subreddit. */
 class PhotoGalleryViewModel : ViewModel() {
 
-    private val _photos = mutableListOf<Photo>()
-    val photos: List<Photo>
-        get() = _photos
+    /** The current text to search. */
+    private val keyword = MutableStateFlow("")
 
-    fun searchPhotos(keyword: String) {
-        // TODO: Simulate fetching data from Reddit API
-        viewModelScope.launch {
-            val result = fetchDataFromReddit(keyword)
-            _photos.clear()
-            _photos.addAll(result)
+    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    val photos: LiveData<UiResult<List<Photo>>> = keyword
+        // Discard texts typed in a very short time to avoid many network calls.
+        .debounce(DEBOUNCE_MILLIS)
+        // Filter out empty text to avoid unnecessary network call.
+        .filter { text ->
+            text.isNotEmpty()
         }
+        // When a new text is set then trigger a network call.
+        .flatMapLatest { text ->
+            fetchDataFromReddit(text)
+        }
+        // Create a LiveData from Flow.
+        .asLiveData()
+
+    fun searchPhotos(keywordToSearch: String) {
+        keyword.value = keywordToSearch
     }
 
-    private suspend fun fetchDataFromReddit(keyword: String): List<Photo> {
+    // TODO: Create a repository class and move this function there, for now return a hardcoded list.
+    private fun fetchDataFromReddit(keyword: String): Flow<UiResult<List<Photo>>> = flow {
         // TODO: Simulate network call to Reddit API.
-        return withContext(Dispatchers.IO) {
-            // TODO: Implement logic to fetch data from Reddit API here,
-            //  for now use this placeholder implementation by returning a hardcoded list of photos.
-            val placeholderPhotos = listOf(
-                Photo("https://via.placeholder.com/150", "Photo 1"),
-                Photo("https://via.placeholder.com/150", "Photo 2"),
-                Photo("https://via.placeholder.com/150", "Photo 3"),
-                Photo("https://via.placeholder.com/150", "Photo 4"),
-                Photo("https://via.placeholder.com/150", "Photo 5"),
-            )
-            placeholderPhotos
-        }
+        val placeholderPhotos = listOf(
+            Photo("https://via.placeholder.com/150", "Photo 1"),
+            Photo("https://via.placeholder.com/150", "Photo 2"),
+            Photo("https://via.placeholder.com/150", "Photo 3"),
+            Photo("https://via.placeholder.com/150", "Photo 4"),
+            Photo("https://via.placeholder.com/150", "Photo 5")
+        )
+        emit(UiResult.successOrEmpty(placeholderPhotos))
+    }
+
+    private companion object {
+        const val DEBOUNCE_MILLIS = 700L
     }
 }
